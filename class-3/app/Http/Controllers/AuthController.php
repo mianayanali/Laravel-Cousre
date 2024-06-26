8c74a4a2f5c0150de9602771736a6dc76886c1a8
mian ayan ali <mianayan911@gmail.com>
2024-06-26 15:47:32 +0500

    auth application

---
+++ class-3/app/Http/Controllers/AuthController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index()
+    {
+        return view('login');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function registration()
+    {
+        return view('registration');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('dashboard')
+                        ->withSuccess('You have Successfully loggedin');
+        }
+
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postRegistration(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|max:8',
+        ]);
+
+        $data = $request->all();
+        $check = User::create($data);
+
+        return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+
+        return redirect("login")->withSuccess('Opps! You do not have access');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function logout() {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+
+    public function homepage()
+    {
+        if(Auth::check()){
+            return view('homepage');
+        }
+
+        return redirect("login")->withSuccess('Opps! You do not have access');
+    }
+}
---
+++ class-3/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post');
+Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('homepage', [AuthController::class, 'homepage'])->name('homepage');
+
